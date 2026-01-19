fe882ef1db1b097799d7e96e0d3e75022a6d6943
prashant mahadik <mahadikprashant4@gmail.com>
2026-01-19 15:43:52 +0530

    2nd commit

--- test_cases/full_shopping.py
+++ test_cases/full_shopping.py
@@ -22,7 +22,6 @@ class Test_03_FullShopping():
     lastname = "Mahadik"
     Zip_code = "411038"
 
-    @pytest.mark.sanity
     def test_full_shopping(self,setup):
         self.logger.info("****************test_full_shopping************")
         self.driver = setup
@@ -59,40 +58,39 @@ class Test_03_FullShopping():
         self.driver.quit()
         self.logger.info("******************shopping done *************")
 
-    @pytest.mark.regression
-    def test_full_shopping111(self, setup):
-        self.logger.info("****************test_full_shopping************")
-        self.driver = setup
-
-        self.driver.implicitly_wait(10)
-        self.driver.get(self.admin_page_url)
-        print("url open")
-        self.driver.maximize_window()
-        self.admin_lp = Login_Admin_Page(self.driver)
-        self.admin_lp.enter_username(self.username)
-        self.admin_lp.enter_password(self.password)
-        self.admin_lp.click_login()
-        print("login success 111111")
-        self.title = self.driver.title
-        if self.title == "Swag Labs":
-            print("title is equal to Swag Labs 1111111")
-            self.logger.info(" ****************test_full_shopping Login success ***************")
-        else:
-            print("title is equal to Swag Labs")
-            self.logger.info(" ****************test_full_shopping Login unsuccess ***************")
-
-        self.shopping_cart = Item_shopping(self.driver)
-        self.shopping_cart.Add_to_cart()
-        self.logger.info(" ****************test_full_shopping add to cart success ***************")
-        self.shopping_cart.cart_btn()
-        print("product added to cart success")
-        self.logger.info(" ****************test_full_shopping cart button click success ***************")
-        self.shopping_cart.checkout_btn()
-        print("product checkout success")
-        self.checkout_info = Checkout_Your_Information(self.driver)
-        self.checkout_info.checkout_your_information(self.firstname, self.lastname, self.Zip_code)
-        print("product checkout info success")
-        print("shopping done")
-        self.driver.quit()
-        self.logger.info("******************shopping done *************")
+    # def test_full_shopping111(self, setup):
+    #     self.logger.info("****************test_full_shopping************")
+    #     self.driver = setup
+    #
+    #     self.driver.implicitly_wait(10)
+    #     self.driver.get(self.admin_page_url)
+    #     print("url open")
+    #     self.driver.maximize_window()
+    #     self.admin_lp = Login_Admin_Page(self.driver)
+    #     self.admin_lp.enter_username(self.username)
+    #     self.admin_lp.enter_password(self.password)
+    #     self.admin_lp.click_login()
+    #     print("login success 111111")
+    #     self.title = self.driver.title
+    #     if self.title == "Swag Labs":
+    #         print("title is equal to Swag Labs 1111111")
+    #         self.logger.info(" ****************test_full_shopping Login success ***************")
+    #     else:
+    #         print("title is equal to Swag Labs")
+    #         self.logger.info(" ****************test_full_shopping Login unsuccess ***************")
+    #
+    #     self.shopping_cart = Item_shopping(self.driver)
+    #     self.shopping_cart.Add_to_cart()
+    #     self.logger.info(" ****************test_full_shopping add to cart success ***************")
+    #     self.shopping_cart.cart_btn()
+    #     print("product added to cart success")
+    #     self.logger.info(" ****************test_full_shopping cart button click success ***************")
+    #     self.shopping_cart.checkout_btn()
+    #     print("product checkout success")
+    #     self.checkout_info = Checkout_Your_Information(self.driver)
+    #     self.checkout_info.checkout_your_information(self.firstname, self.lastname, self.Zip_code)
+    #     print("product checkout info success")
+    #     print("shopping done")
+    #     self.driver.quit()
+    #     self.logger.info("******************shopping done *************")
 
